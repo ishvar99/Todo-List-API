266e5d5b62fc2116e7132252bf9a26ec296365c8
Ishan Varshney <ishanvarshney99@gmail.com>
2019-02-28 01:22:02 +0530

    Added show, update and delete routes

--- helpers/todos.js
+++ helpers/todos.js
@@ -1,4 +1,6 @@
 var Todo=require('../models/todos');
+var {ObjectId} = require('mongoose').Types;
+
 exports.getTodos=function(req,res){
   Todo.find()
   .then((todos)=>{
@@ -6,7 +8,6 @@ exports.getTodos=function(req,res){
   },(e)=>{
   	console.log(e);
   })
-
 }
 exports.postTodo=function(req,res){
 	var body=req.body;
@@ -16,6 +17,50 @@ exports.postTodo=function(req,res){
 	})
 	.catch((error)=>{
         console.log(error);
+	})
+}
+exports.getTodo=function(req,res){
+	var id=req.params.id;
+	if(!ObjectId.isValid(id)){
+		return res.status(400).send('Bad Request!')
+	}
+	Todo.findOne({_id:id})
+	.then((todo)=>{
+		if(!todo)
+			throw new Error('Todo not found!')
+		res.json(todo);
+	})
+	.catch((error)=>{
+		res.status(404).send(error.message)
+	})
+}
+exports.updateTodo=function(req,res){
+	 var id=req.params.id;
+	 if(!ObjectId.isValid(id))
+	 	return res.status(400).send('Bad Request!')
+	 var body=req.body;
+	 Todo.findOneAndUpdate({_id:id},body,{new:true})
+	 .then((updatedTodo)=>{
+	 	  if(!updateTodo)
+	 	  	throw new Error('Todo not found!')
+           res.json(updatedTodo);
+	 })
+	 .catch((error)=>{
+           res.status(404).send(error.message);
+	 })
+}
+exports.deleteTodo=function(req,res){
+	var id=req.params.id;
+	if(!ObjectId.isValid(id))
+		return res.status(400).send('Bad Request')
+	Todo.findOneAndRemove({_id:id})
+	.then((deletedTodo)=>{
+		if(!deletedTodo)
+			throw new Error('Todo not found!')
+		res.redirect('/todos');
+	})
+	.catch((error)=>{
+		res.status(404).send(error.message)
 	})
 }
 module.exports=exports;
--- routes/todos.js
+++ routes/todos.js
@@ -4,4 +4,8 @@ var router=express.Router();
 router.route('/')
 .get(helpers.getTodos)
 .post(helpers.postTodo)
+router.route('/:id')
+.get(helpers.getTodo)
+.put(helpers.updateTodo)
+.delete(helpers.deleteTodo)
 module.exports=router;
